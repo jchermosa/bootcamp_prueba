3a76a15978b9f234fc007506206f8bcbe8ce5ddb
Julio Hermosa <jhermosa@roshka.com>
2025-03-11 10:22:57 -0300

    add tableview

---
+++ prueba/CellTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CellTableViewCell.swift
+//  prueba
+//
+//  Created by Julio Hermosa on 2025-03-11.
+//
+
+import UIKit
+
+class CellTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
